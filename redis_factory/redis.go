52f3759a1b8606fbcbc45a9626bdc71db8928244
fly <13668266758@139.com>
2021-12-28 10:35:53 +0800

    change: redis 连接池

--- redis_factory/redis.go
+++ redis_factory/redis.go
@@ -34,8 +34,9 @@ func CreateRedis(nodes []string) *RedisClient {
 		} else if len(nodes) >= 6 {
 			mode = 6
 			clusterClient = redis.NewClusterClient(&redis.ClusterOptions{
-				Addrs:        nodes,
-				MaxRedirects: 8, // 当遇到网络错误或者MOVED/ASK重定向命令时，最多重试几次，默认8
+				Addrs:         nodes,
+				RouteRandomly: true,
+				MaxRedirects:  8, // 当遇到网络错误或者MOVED/ASK重定向命令时，最多重试几次，默认8
 				// 超时
 				DialTimeout:  5 * time.Second, // 连接建立超时时间，默认5秒
 				ReadTimeout:  3 * time.Second, // 读超时，默认3秒， -1表示取消读超时
@@ -43,15 +44,15 @@ func CreateRedis(nodes []string) *RedisClient {
 				PoolTimeout:  4 * time.Second, // 当所有连接都处在繁忙状态时，客户端等待可用连接的最大等待时长，默认为读超时+1秒
 				// 闲置连接检查包括IdleTimeout，MaxConnAge
 				IdleCheckFrequency: 60 * time.Second, // 闲置连接检查的周期，默认为1分钟，-1表示不做周期性检查，只在客户端获取连接时对闲置连接进行处理
-				IdleTimeout:        1 * time.Minute,  // 闲置超时，默认5分钟，-1表示取消闲置超时检查
-				MaxConnAge:         0 * time.Second,  // 连接存活时长，从创建开始计时，超过指定时长则关闭连接，默认为0，即不关闭存活时长较长的连接
+				// IdleTimeout:        1 * time.Minute,  // 闲置超时，默认5分钟，-1表示取消闲置超时检查
+				MaxConnAge: 0 * time.Second, // 连接存活时长，从创建开始计时，超过指定时长则关闭连接，默认为0，即不关闭存活时长较长的连接
 				// 命令执行失败时的重试策略
 				MaxRetries:      0,                      // 命令执行失败时，最多重试多少次，默认为0即不重试
 				MinRetryBackoff: 8 * time.Millisecond,   // 每次计算重试间隔时间的下限，默认8毫秒，-1表示取消间隔
 				MaxRetryBackoff: 512 * time.Millisecond, // 每次计算重试间隔时间的上限，默认512毫秒，-1表示取消间隔
 				// 连接池
-				PoolSize:     20, // 连接池最大socket连接数，默认为5倍CPU数， 5 * runtime.NumCPU
-				MinIdleConns: 20, // 在启动阶段创建指定数量的Idle连接，并长期维持idle状态的连接数不少于指定数量
+				PoolSize:     5, // 连接池最大socket连接数，默认为5倍CPU数， 5 * runtime.NumCPU
+				MinIdleConns: 5, // 在启动阶段创建指定数量的Idle连接，并长期维持idle状态的连接数不少于指定数量
 			})
 		} else {
 			mode = 1
